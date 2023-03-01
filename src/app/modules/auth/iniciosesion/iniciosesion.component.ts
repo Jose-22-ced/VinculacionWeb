2603c7e63fbd2e8118b7e7d0bba76c0a69732243
jose22gar <jose.cedeno@consulti.ec>
2023-02-28 20:22:49 -0500

    Produccion

--- src/app/modules/auth/iniciosesion/iniciosesion.component.ts
+++ src/app/modules/auth/iniciosesion/iniciosesion.component.ts
@@ -7,117 +7,124 @@ import {Router} from "@angular/router";
 import {IniciosesionService} from "../../../services/iniciosesion.service";
 import {SocialAuthService} from "@abacritt/angularx-social-login";
 
-let PARAMETROS=''
+let PARAMETROS = ''
+
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: UntypedFormControl | null, form: FormGroupDirective | NgForm | null): boolean {
     const isSubmitted = form && form.submitted;
     return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
   }
 }
+
 @Component({
   selector: 'app-iniciosesion',
   templateUrl: './iniciosesion.component.html',
   styleUrls: ['./iniciosesion.component.css']
 })
-export class IniciosesionComponent implements OnInit,AfterViewInit {
+export class IniciosesionComponent implements OnInit, AfterViewInit {
   //Angular Social Login
-  user:any;
-  loggedIn:any;
-  issloading=true;
- //Obtiene los datos del inicio de sesión
+  user: any;
+  loggedIn: any;
+  issloading = true;
+  //Obtiene los datos del inicio de sesión
   public userRequest: User = new User();
   //Habilita ek incio o el cierre de sesión
   habilitar: boolean = true;
 
   //Validaciones
-  cedulaFormControl = new UntypedFormControl('', [ Validators.pattern( '[0-9]{10}'),Validators.required]);
+  cedulaFormControl = new UntypedFormControl('', [Validators.pattern('[0-9]{10}'), Validators.required]);
   matcher = new MyErrorStateMatcher();
-  omit_special_char(event: { charCode: any; })
-  {var k;
+
+  omit_special_char(event: { charCode: any; }) {
+    var k;
     k = event.charCode;  //         k = event.keyCode;  (Both can be used)
-    return((k >= 48 && k <= 57));
+    return ((k >= 48 && k <= 57));
   }
-  omit_max_char(event:{ target: any; })
-  {var k;
+
+  omit_max_char(event: { target: any; }) {
+    var k;
     k = event.target.value.length;  //         k = event.keyCode;  (Both can be used)
     console.log(k)
     return (k <= 9);
   }
-  constructor(private router: Router,private authService: SocialAuthService,private iniciosesionService: IniciosesionService) { }
-  ngAfterViewInit(): void {
-    setTimeout(()=>{
-      this.issloading=false;
-    },2000)
+
+  constructor(private router: Router, private authService: SocialAuthService, private iniciosesionService: IniciosesionService) {
   }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.issloading = false;
+    }, 2000)
+  }
+
   ngOnInit(): void {
     this.authService.authState.subscribe((user) => {
       this.user = user;
       this.loggedIn = (user != null);
-      console.log(this.user)
+      this.userRequest.email = this.user.email;
+      this.userRequest.urlFoto = this.user.photoUrl;
+      this.userRequest.rol = '';
+      this.iniciosesionService.Login(this.userRequest).subscribe(
+        data => {
+          sessionStorage.clear;
+          if (data.rol == "DOC") {
+            Swal.fire({
+              title: 'Error',
+              text: 'La cédula ingresada no pertenece al Instituto Superior Tecnológico del Azuay',
+              icon: 'warning',
+              color: "#0c3255",
+              confirmButtonColor: "#0c3255",
+              background: "#fbc02d",
+            })
+          } else {
+            sessionStorage.setItem('user', JSON.stringify(data));
+            this.router.navigate(['/panelusuario/proyectovinculacion/bienvenida']);
+          }
+        },
+        err => {
+          if (err.error.mensaje == "No existe") {
+            this.setHabilitar(false);
+          }
+        }
+      )
     });
   }
-  logOut(): void{
+
+  logOut(): void {
 
   }
+
   //Auth2 para el incio de sesón con google.
   signInWithGoogle(): void {
-    /*this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then(
-      data => {
-        this.userRequest.email=data.email;
-        this.userRequest.urlFoto=data.photoUrl;
-        this.userRequest.rol='';
-        this.iniciosesionService.Login(this.userRequest).subscribe(
-          data=>{
-            sessionStorage.clear;
-            if(data.rol=="DOC"){
-              Swal.fire({
-                title: 'Error',
-                text: 'La cédula ingresada no pertenece al Instituto Superior Tecnológico del Azuay',
-                icon: 'warning',
-                color: "#0c3255",
-                confirmButtonColor:"#0c3255",
-                background: "#fbc02d",
-              })
-            }else{
-              sessionStorage.setItem('user', JSON.stringify(data));
-              this.router.navigate(['/panelusuario/proyectovinculacion/bienvenida']);
-            }
-          },
-          err =>{
-            if(err.error.mensaje=="No existe"){
-              this.setHabilitar(false);
-            }
-          }
-        )
-      }
-    )*/
+
   }
+
   //Crea a un usario nuevo si este no existe
-  public create():void{
+  public create(): void {
     this.iniciosesionService.Signup(this.userRequest).subscribe(
       data => {
         sessionStorage.clear;
-        if(data.rol=="DOC"){
+        if (data.rol == "DOC") {
           Swal.fire({
             title: 'Error',
             text: 'La cédula ingresada no pertenece al Instituto Superior Tecnológico del Azuay',
             icon: 'warning',
             color: "#0c3255",
-            confirmButtonColor:"#0c3255",
+            confirmButtonColor: "#0c3255",
             background: "#fbc02d",
           })
-        }else{
+        } else {
           sessionStorage.setItem('user', JSON.stringify(data));
           this.router.navigate(['/panelusuario/proyectovinculacion/bienvenida']);
         }
       },
-      err=>{
+      err => {
         Swal.fire({
           title: 'Error',
           text: 'La cédula ingresada no pertenece al Instituto Superior Tecnológico del Azuay',
           icon: 'warning',
           color: "#0c3255",
-          confirmButtonColor:"#0c3255",
+          confirmButtonColor: "#0c3255",
           background: "#fbc02d",
         })
       }
@@ -125,7 +132,7 @@ export class IniciosesionComponent implements OnInit,AfterViewInit {
   }
 
   //Metodo de ocultar y mostrar componetes.
-  setHabilitar(habilitar:boolean):void{
-    this.habilitar=(this.habilitar==true)? false: true;
+  setHabilitar(habilitar: boolean): void {
+    this.habilitar = (this.habilitar == true) ? false : true;
   }
 }
